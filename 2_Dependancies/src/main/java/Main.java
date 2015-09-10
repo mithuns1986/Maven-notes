af8300d619eac9ed4beda384437cf0772abb97c6
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-09-10 15:43:46 +1200

    2_Dependencies added

---
+++ 2_Dependancies/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by akshayrajgollahalli on 10/09/15.
+ */
+import org.joda.time.DateTime;
+public class Main {
+    public static void main(String[] args) {
+        DateTime time = new DateTime();
+        System.out.println(time);
+    }
+}
